5d39118eeba3f2f5bf421e2cee4715c027b236f5
FJBeltranPujante <kvortvaj@gmail.com>
2024-05-26 11:16:10 +0200

    service update to search for heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -9,6 +9,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { HeroImagePipe } from './pipes/heroImage.pipe';
 
 
 @NgModule({
@@ -18,7 +19,10 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
-    CardComponent
+    CardComponent,
+
+    //pipes
+    HeroImagePipe
   ],
   imports: [
     CommonModule,
--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-hero-page',
@@ -6,6 +10,27 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+
+  public Hero?: Hero;
+
+  constructor(
+    private HeroesService: HeroesService,
+    private ActivatedRoute: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+    ngOnInit(): void {
+      this.ActivatedRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.HeroesService.getHeroById(id)),
+      ).subscribe ( hero => {
+        if ( !hero ) return this.router.navigate([ '/heroes/list' ]);
+
+        this.Hero = hero;
+        return;
+      })
+
+    }
 
 }
